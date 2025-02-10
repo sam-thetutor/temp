ea3ba762ffef325e81fd7d455a87d9eea1485775
sam-thetutor <atwebembeiresamuel2020@gmail.com>
2025-02-10 19:47:01 +0300

    g

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
     <>
       {/* <LandingPage/> */}
       {/* <LogoutPage/> */}
-      <Profile/>
-      {/* <Dashboard/> */}
+      {/* <Profile/> */}
+      <Dashboard/>
     </>
   )
 }
